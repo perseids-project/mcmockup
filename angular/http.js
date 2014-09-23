169cad954c0e1aab959aea7fb54df8d71e0894d1
caesarfeta <adamtavares@gmail.com>
2014-09-23 16:10:34 -0400

    Added support for markdown.

--- angular/http.js
+++ angular/http.js
@@ -6,15 +6,29 @@ app.service( 'http', function( $http, $q ) {
 	});
 	
 	function gimme( scope ) {
-		var request = jackson( 'GET', scope.data_src, {} );
+		var request = client( 'GET', scope.data_src, {} );
 		return( request.then(
-			function( r ) { return r.data },
+			function( r ) { return md(r.data) },
 			function( r ) { return r.data.error }
 		));
 	}
+	
+	function md( data ) {
+		var conv = new Showdown.converter();
+		
+		// What data needs conversion to markdown?
+		data.commentary.body = conv.makeHtml( data.commentary.body );
+		data.commentary.bibl = conv.makeHtml( data.commentary.bibl );
+		data.original = conv.makeHtml( data.original );
+		for ( var lang in data.translation ) {
+			data.translation[lang] = conv.makeHtml( data.translation[lang] );
+		}
+		
+		return data;
+	}
 		
 	// JackSON wrapper
-	function jackson( method, url, data ) {
+	function client( method, url, data ) {
 		return $http({
 			method: method.toUpperCase(),
 			url: url,
@@ -28,5 +42,7 @@ app.service( 'http', function( $http, $q ) {
 	// JackSON formatted json
 	function wrap( json ) {
 		return { data: json }
-	}	
+	}
+	
+	
 });
