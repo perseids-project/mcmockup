c373eb188c94a409ee7640408c09d184e6a3cfc8
caesarfeta <adamtavares@gmail.com>
2014-09-23 15:00:53 -0400

    Improved content toggle switches.

--- angular/controller.js
+++ angular/controller.js
@@ -9,4 +9,21 @@ app.controller("controller", function( $scope, http ){
 	
 	// Messages
 	$scope.msg = "Welcome!";
+	
+	// Language toggle
+	$scope.lang = "eng";
+	$scope.lang_switch = function( lang ) {
+		$scope.lang = lang;
+	}
+	$scope.lang_check = function( lang ) {
+		return ( $scope.lang == lang )?true:false;
+	}
+	
+	$scope.comment = "body";
+	$scope.comment_switch = function() {
+		$scope.comment = ( $scope.comment == 'body' ) ? 'bibl':'body';
+	}
+	$scope.comment_check = function( view ) {
+		return ( $scope.comment == view )?true:false;
+	}
 });
